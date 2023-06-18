9d040da60fb874a49d1317c749ff6d9f445c2a52
lucmanm <greenjamez22@gmail.com>
2023-06-18 22:16:09 +0300

    REgular Upates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,29 @@
+import { footerInfo } from "@/constant";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+  <footer className=" bg-slate-100">
+    <div className="dt:container flex flex-wrap gap-2">
+      <div className="flex">
+        <div className="">Logo</div>
+        <div className="flex gap-2">
+          {
+          footerInfo.map(({title, info})=>(
+            <div key={title} className="font-semibold">
+              <span>{info.title.[1][1]}</span>
+            </div>
+          ))
+          }
+        </div>
+      </div>
+    </div>
+    <div className="bg-blue-950">
+      <div className="flex-grow min-w-max text-white p-1 dt:container text-center">
+        <span>COPYRIGHT © 2022. COMPUTECH COMPANY. C. R. NO. 4030079509 VAT ID 311079231900003. ALL RIGHTS RESERVED.</span>
+      </div>
+    </div>
+  </footer>)
 };
 
 export default Footer;

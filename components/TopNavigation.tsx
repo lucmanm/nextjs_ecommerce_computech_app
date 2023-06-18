81df71bd4407492e1e1f52f9a393a72e886c0f98
lucmanm <greenjamez22@gmail.com>
2023-06-18 13:35:47 +0300

    Regular Updates

--- components/TopNavigation.tsx
+++ components/TopNavigation.tsx
@@ -13,13 +13,13 @@ const TopNavigation = ({
     <div className="relative dt:container flex flex-wrap justify-between">
       <div className="flex">
         {topNavigationMenu.map(({ title, url, imageSrc, altImage }) => (
-          <div key={title} className="p-2 border flex font-medium gap-x-2">
+          <div key={title} className="p-2 border flex gap-x-2">
             <Image src={imageSrc} width={18} height={18} alt={altImage} />
             <span>{title}</span>
           </div>
         ))}
       </div>
-      <div className="p-2 border flex font-medium gap-x-2">
+      <div className="p-2 border flex gap-x-2">
         <Image src="language.svg" width={18} height={18} alt="language" />
         <span>langage</span>
       </div>

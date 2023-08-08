63aef483b8f1410cf0848a875869f6464cdd7342
lucmanm <greenjamez22@gmail.com>
2023-08-08 14:06:38 +0300

    ...

--- types/index.ts
+++ types/index.ts
@@ -1,48 +1,44 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-  btnType?: "button" | "submit";
-  clickHandler?: MouseEventHandler<HTMLButtonElement>;
-  icon?: string;
-  title?: string;
-  customButtonStyle?: string;
+    btnType?: "button" | "submit";
+    clickHandler?: MouseEventHandler<HTMLButtonElement>;
+    icon?: string;
+    title?: string;
+    customButtonStyle?: string;
 }
 
 export interface TopNavigationProps {
-  title?: string;
-  url?: string;
-  imageSrc?: string;
-  altImage?: string;
+    title?: string;
+    url?: string;
+    imageSrc?: string;
+    altImage?: string;
 }
 
 export interface FooterInfoProps {
-  title?: string;
-  info?: string;
-  url?: string;
+    title?: string;
+    info?: string;
+    url?: string;
 }
 
 export interface paymentMethodProps {
-  title?: string;
+    title?: string;
 }
 export interface ListProps {
-  description: string;
-  imgLink: string;
-  url: string;
+    description: string;
+    imgLink: string;
+    url: string;
 }
 export interface ProductCategoryProps {
-  title?: string;
-  list: ListProps[];
-<<<<<<< HEAD
-=======
-  link: string;
->>>>>>> 4a05972bee8f82f5a9a711e90783f6da06c72d80
+    title?: string;
+    list: ListProps[];
 }
 
 export interface SliderProps {
-  sliderhead?: string;
-  list: ListProps[];
-  subtitle?: boolean;
-  sliderSlide?: string;
-  sliderCustomerStyle?: string;
-  arrow?: boolean;
+    sliderhead?: string;
+    list: ListProps[];
+    subtitle?: boolean;
+    sliderSlide?: string;
+    sliderCustomerStyle?: string;
+    arrow?: boolean;
 }

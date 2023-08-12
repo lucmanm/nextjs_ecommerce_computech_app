d3485fba26fc9658491da605ba298e8dfc554a9f
lucmanm <greenjamez22@gmail.com>
2023-08-12 10:24:17 +0300

    ...

--- components/block/SignInForm.tsx
+++ components/block/SignInForm.tsx
@@ -4,79 +4,80 @@ import { z } from "zod";
 import { Button } from "../ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "../ui/form";
 import { Input } from "../ui/input";
 
 const formSchema = z.object({
-    email: z
-        .string()
-        .min(1, "Please Enter your Email")
-        .email("Invalid Email address"),
-    password: z
-        .string()
-        .min(1, "Please enter your password")
-        .min(8, "Password Must  atleast 8 characters"),
+  email: z
+    .string()
+    .min(1, "Please Enter your Email")
+    .email("Invalid Email address"),
+  password: z
+    .string()
+    .min(1, "Please enter your password")
+    .min(8, "Password Must  atleast 8 characters"),
 });
 
 const SignInForm = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
-    }
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="mx-auto my-20 w-full space-y-8 rounded-md border-black bg-white p-4 shadow-md md:w-1/3">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Email" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder="Enter Your Passord"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit" size={"sm"} variant="default">
-                    Sign In
-                </Button>
-            </form>
-        </Form>
-    );
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <div className="mx-auto my-20 w-full space-y-8 rounded-md border-black bg-white p-4 shadow-md md:w-1/3">
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Passord" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" size={"sm"} variant="default">
+            Sign In
+          </Button>
+        </form>
+        <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block  after:h-px after:flex-grow after:bg-stone-400">
+          or
+        </div>
+        <p>If you don&apos;t have an account, please&mbsp;.</p>
+      </div>
+    </Form>
+  );
 };
 
 export default SignInForm;

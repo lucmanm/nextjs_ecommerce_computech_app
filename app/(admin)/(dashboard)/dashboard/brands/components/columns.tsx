28be14179e273b5acbd759e6e23387cabe47e7e7
lucmanm <greenjamez22@gmail.com>
2023-09-10 13:38:27 +0300

    ...

--- app/(admin)/(dashboard)/dashboard/brands/components/columns.tsx
+++ app/(admin)/(dashboard)/dashboard/brands/components/columns.tsx
@@ -2,6 +2,7 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import CellAction from "./cell-action";
+import Image from "next/image";
 
 export type BrandColumnProps = {
   id: string;
@@ -18,8 +19,17 @@ export const columns: ColumnDef<BrandColumnProps>[] = [
   {
     accessorKey: "imageUrl",
     header: "Image",
+    cell: ({ row }) => (
+      <div className="">
+        <Image
+          alt={row.original.brand}
+          src={row.original.imageUrl}
+          width={50}
+          height={50}
+        />
+      </div>
+    ),
   },
-
   {
     accessorKey: "createdAt",
     header: "Date Created",

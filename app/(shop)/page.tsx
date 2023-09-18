abc007a1be5061175b00ceeb6335e7223c14cedb
lucmanm <greenjamez22@gmail.com>
2023-09-18 13:06:49 +0300

    Test #2

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,6 +1,6 @@
-import Slider from "@/components/Slider";
-import CarouselSlider from "@/components/CarouselSlider";
-import { getSliders } from "@/lib/actions/getSliders";
+// import Slider from "@/components/Slider";
+// import CarouselSlider from "@/components/CarouselSlider";
+// import { getSliders } from "@/lib/actions/getSliders";
 // import BrandSlider from "./components/brand-slider";
 // import { getBrands } from "@/lib/actions/getBrands";
 // import HorizontalProductList from "@/components/horizontal-products-list";
@@ -9,15 +9,14 @@ import { getSliders } from "@/lib/actions/getSliders";
 export const revalidate = 0;
 
 export default async function ShopPage() {
-  const slider = await getSliders();
+  // const slider = await getSliders();
   // const brands = await getBrands();
   // const products = await getProducts();
 
   return (
     <div className="my-4 flex flex-col space-y-4 overflow-hidden md:my-8 md:space-y-8">
       {/* Main slider for homepage */}
-      <CarouselSlider sliderData={slider} />
-
+      {/* <CarouselSlider sliderData={slider} /> */}
       {/* all Product Type  Data  - import Slider Components  */}
       {/* Disabled / Hidden */}
       {/* <Slider
@@ -25,11 +24,11 @@ export default async function ShopPage() {
           list={productList}
           sliderCustomerStyle="rounded-full w-20 h-20 dt:w-28 dt:h-28" //Csutom style for slider
         /> */}
-
+      Shop Page Data
       {/* Category of products display */}
       {/* <div className="px-3 lg:px-0">
         <HorizontalProductList title="Featured" productData={products} />
-
+        
         <BrandSlider
           subtitle={false}
           sliderhead="Brand"

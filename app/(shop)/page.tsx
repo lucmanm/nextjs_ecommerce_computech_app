255a74cb123a33bf699aca8fa06230788b4e294f
lucmanm <greenjamez22@gmail.com>
2023-09-18 17:50:09 +0300

    Slider Enabled

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,7 +1,7 @@
 // import Slider from "@/components/Slider";
-// import CarouselSlider from "@/components/CarouselSlider";
-// import { getSliders } from "@/lib/actions/getSliders";
-// import { SliderProps } from "@/types/table-types";
+import CarouselSlider from "@/components/CarouselSlider";
+import { getSliders } from "@/lib/actions/getSliders";
+import { SliderProps } from "@/types/table-types";
 import BrandSlider from "./components/brand-slider";
 import { getBrands } from "@/lib/actions/getBrands";
 import HorizontalProductList from "@/components/horizontal-products-list";
@@ -10,14 +10,14 @@ import { getProducts } from "@/lib/actions/getProduct";
 export const revalidate = 0;
 
 export default async function ShopPage() {
-  // const slider: SliderProps[] = await getSliders();
+  const slider: SliderProps[] = await getSliders();
   const brands = await getBrands();
   const products = await getProducts();
 
   return (
     <div className="my-4 flex flex-col space-y-4 overflow-hidden md:my-8 md:space-y-8">
       {/* Main slider for homepage */}
-      {/* <CarouselSlider sliderData={slider} /> */}
+      <CarouselSlider sliderData={slider} />
       Shop Home page
       {/* all Product Type  Data  - import Slider Components  */}
       {/* Disabled / Hidden */}

f262ea16abb1a2d24be93e8dc0cdc857e764e45f
lucmanm <greenjamez22@gmail.com>
2023-12-14 01:48:09 +0300

    updates and changes

--- app/(shop)/product/[productSlug]/components/fitlers/brand.tsx
+++ app/(shop)/product/[productSlug]/components/fitlers/brand.tsx
@@ -1,9 +1,19 @@
+import { Checkbox } from "@/components/ui/checkbox";
+
 const BrandFilter = () => {
-    return ( 
-        <div>
-            Brand Filter
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms2"  checked/>
+        <label
+          htmlFor="terms2"
+          className="font-medium"
+        >
+          Dell
+        </label>
+      </div>
+    </div>
+  );
+};
+
 export default BrandFilter;

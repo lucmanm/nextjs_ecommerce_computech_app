b66fa952cda8dbf8cfa03f78b11809f3da8fc669
lucmanm <greenjamez22@gmail.com>
2023-12-16 20:21:37 +0300

    Bug Fixes

--- app/(admin)/(route)/dashboard/orders/page.tsx
+++ app/(admin)/(route)/dashboard/orders/page.tsx
@@ -1,5 +1,4 @@
-import Navigation from "@/app/(admin)/(route)/dashboard/components/Navigation";
-import Sidebar from "@/app/(admin)/components/Sidebar";
+
 
 const OrdersPage = () => {
     return <main className="flex">asdas</main>;

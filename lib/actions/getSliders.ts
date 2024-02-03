7ca9521e84c04f35ca273d5ce2e8bea8aabf5042
lucmanm <greenjamez22@gmail.com>
2024-02-03 11:12:35 +0300

    Update #030224

--- lib/actions/getSliders.ts
+++ lib/actions/getSliders.ts
@@ -1,7 +1,5 @@
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/sliders`;
-
 export async function getSliders() {
-  const res = await fetch(`${URL}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sliders`);
   if (!res.ok) {
     throw new Error("FAILED_FETCH_SLIDERS");
   }
